774bc1c8880eb905c94950e86649e520aa75671e
Konstantin-Sinitsyn <sinizin2000@gmail.com>
2020-08-21 15:47:43 +0300

    portfolio-site

---
+++ Portfolio/js/main.js
@@ -0,0 +1,10 @@
+new WOW().init();
+
+var mySwiper = new Swiper('.swiper-container', {
+        pagination: {
+          el: '.projects-pagination',
+          bulletClass:"projects-bullet",
+          bulletAcviteClass:"projects-bullet-active",
+          clickable:true
+        },
+      });
